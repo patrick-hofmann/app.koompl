a3c85c290d715ee6ebb967a609a2572fe8051c90
Patrick Hofmann <phofmann@delta-mind.at>
2025-10-02 11:23:53 +0200

    refactor: Disable various stylistic ESLint rules to simplify configuration and improve flexibility in code formatting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,16 +11,18 @@ export default withNuxt({
     'vue/multiline-html-element-content-newline': 'off',
     'vue/html-closing-bracket-newline': 'off',
     'no-trailing-spaces': ['error', { skipBlankLines: false, ignoreComments: false }],
-    'quotes': ['error', 'single', { avoidEscape: true }],
+    'quotes': 'off',
+    '@stylistic/quotes': 'off',
     'semi': ['error', 'never'],
     'comma-dangle': ['error', 'never'],
-    'operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }],
-    '@stylistic/member-delimiter-style': ['error', {
-      multiline: { delimiter: 'semi', requireLast: false },
-      singleline: { delimiter: 'semi', requireLast: false }
-    }],
+    'operator-linebreak': 'off',
+    '@stylistic/operator-linebreak': 'off',
+    '@stylistic/member-delimiter-style': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    'quote-props': 'off',
+    '@stylistic/quote-props': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: false }],
-    '@stylistic/arrow-parens': ['error', 'as-needed', { requireForBlockBody: false }]
+    'arrow-parens': 'off',
+    '@stylistic/arrow-parens': 'off'
   }
 })
